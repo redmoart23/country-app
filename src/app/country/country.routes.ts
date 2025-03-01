c892c78b301576e116c145a0cd0529593ae362c0
red <redmoart@gmail.com>
2025-03-01 16:02:39 -0500

    🔧Implement routing and create home and by-capital pages

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ByCapitalPageComponent,
+  },
+];
+
+export default routes;
